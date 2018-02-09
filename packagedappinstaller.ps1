cac2d0bc3608ae6e29f6adfb9c61af49589a9c4d
tim-beer <tim@fignon.co.uk>
2018-02-09 12:08:44 +0000

    Add files via upload

---
+++ packagedappinstaller.ps1
@@ -0,0 +1,16 @@
+﻿$archive = 'C:\Windows\Temp\AdobeReader2017.zip'
+$Install = 'C:\Windows\Temp\AdobeReader'
+$url = 'insert your url including the ?SV part'
+Invoke-WebRequest $url -OutFile $archive -UseBasicParsing
+Expand-Archive -Path $archive -DestinationPath $Install
+$Install = 'C:\Windows\Temp\AdobeReader'
+$args = @(
+"/I" 
+"$Install\AcroRead.msi"
+"TRANSFORMS=$Install\AcroRead.mst"
+"/Q"
+)
+Start-Process msiexec.exe -Wait -ArgumentList $args
+Remove-Item -path $archive -Recurse
+
+Remove-Item -path $Install -Recurse
